1b72b125e1f1fb508a0b5064bcfa9abff2be2c65
Usaid274 <usaidhasan000@gmail.com>
2024-10-18 13:35:07 +0500

    add number counter animation

---
+++ app/components/home/number-counter.jsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useEffect } from "react";
+import { motion, useAnimate, useInView, useMotionValue, useTransform } from "framer-motion";
+import Section2Img from "@/app/assets/home/section2-1.png";
+
+export default function NumberCounter({ }) {
+  const [scope, animate] = useAnimate();
+  const isInView = useInView(scope, { once: true })
+
+  const counter1 = useMotionValue(10);
+  const counter1Formatted = useTransform(counter1, (x) => `${Math.round(x)}+` );
+
+  const counter2 = useMotionValue(10);
+  const counter2Formatted = useTransform(counter2, (x) => `${Math.round(x)}+` );
+
+  const counter3 = useMotionValue(10);
+  const counter3Formatted = useTransform(counter3, (x) => `${Math.round(x)}+` );
+
+  const counter4 = useMotionValue(10);
+  const counter4Formatted = useTransform(counter4, (x) => `${Math.round(x)}%` );
+
+  useEffect(() => {
+    if (isInView) {
+      const animation = animate([
+        [counter1, 100, { at: "<" }],
+        [counter2, 60, { at: "<" }],
+        [counter3, 300, { at: "<" }],
+        [counter4, 90, { at: "<" }],
+      ], { duration: 3 });
+
+      return animation.stop;
+    }
+  }, [isInView]);
+
+  return (
+    <div ref={scope} className="bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `linear-gradient(to right, #000000a0, #000000a0), url(${Section2Img.src})` }}>
+      <ul className="grid grid-cols-2 gap-6 justify-center items-center h-full max-w-screen-md py-28 pl-28 pr-4 max-lg:px-6 max-lg:justify-items-center max-lg:mx-auto">
+        <li>
+          <motion.div className="text-3xl font-semibold">{counter1Formatted}</motion.div>
+          <div className="text-sm font-medium mt-2">Lorem Ipsum is simply</div>
+        </li>
+        <li>
+          <motion.div className="text-3xl font-semibold">{counter2Formatted}</motion.div>
+          <div className="text-sm font-medium mt-2">Lorem Ipsum is simply</div>
+        </li>
+        <li>
+          <motion.div className="text-3xl font-semibold">{counter3Formatted}</motion.div>
+          <div className="text-sm font-medium mt-2">Lorem Ipsum is simply</div>
+        </li>
+        <li>
+          <motion.div className="text-3xl font-semibold">{counter4Formatted}</motion.div>
+          <div className="text-sm font-medium mt-2">Lorem Ipsum is simply</div>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- app/components/home/section2.jsx
+++ app/components/home/section2.jsx
@@ -1,15 +1,15 @@
-import Section2Img from "@/app/assets/home/section2-1.png";
+import NumberCounter from "@/app/components/home/number-counter";
 
 export default function Section2() {
   return (
     <section className="bg-secondary text-white">
       <div className="grid grid-cols-2 max-lg:grid-cols-1 w-full" >
         <div className="py-12 max-w-screen-md justify-self-end max-lg:justify-self-start">
-          <div className="border-r py-16 pr-28 pl-4 max-lg:border-none max-lg:px-6">
+          <div className="border-r py-16 pr-28 pl-4 max-lg:border-none max-lg:p-6">
             <h2 className="text-4xl font-bold">
               <span className="text-primary">Our Analytics: </span>
-              <span>Crafting Custom Software Solutions </span>
-              <span className="text-primary text-6xl leading-3">. </span>
+              <span>Crafting Custom Software Solutions</span>
+              <span className="text-primary text-6xl leading-3">.</span>
             </h2>
             <p className="font-medium text-sm mt-6">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
@@ -17,26 +17,7 @@ export default function Section2() {
           </div>
         </div>
 
-        <div className="bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `linear-gradient(to right, #000000a0, #000000a0), url(${Section2Img.src})` }}>
-          <ul className="grid grid-cols-2 gap-6 justify-center items-center h-full max-w-screen-md py-28 pl-28 pr-4 max-lg:px-6 max-lg:justify-items-center max-lg:mx-auto">
-            <li>
-              <div className="text-3xl font-bold">3.5k</div>
-              <div className="text-sm font-medium mt-2">Lorem Ipsum is simply</div>
-            </li>
-            <li>
-              <div className="text-3xl font-bold">3.5k</div>
-              <div className="text-sm font-medium mt-2">Lorem Ipsum is simply</div>
-            </li>
-            <li>
-              <div className="text-3xl font-bold">3.5k</div>
-              <div className="text-sm font-medium mt-2">Lorem Ipsum is simply</div>
-            </li>
-            <li>
-              <div className="text-3xl font-bold">3.5k</div>
-              <div className="text-sm font-medium mt-2">Lorem Ipsum is simply</div>
-            </li>
-          </ul>
-        </div>
+      <NumberCounter />
       </div>
     </section>
   );
